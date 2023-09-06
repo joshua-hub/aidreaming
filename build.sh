2246cb034dd6a9d8928ed9becaab5ce5ffeed469
josh <josh@none>
2023-09-06 20:32:17 +1000

    Dockerfile: correct change user position, build.sh: add cuda_malloc function to launch.sh

--- build.sh
+++ build.sh
@@ -20,8 +20,9 @@ fi
 
 git clone https://github.com/lllyasviel/Fooocus.git
 
-#move Cokerfile and begin changing the repo
-cp Dockerfile-aidreaming $repos_dir/Fooocus/
+# move Dockerfile and begin changing the repo
+mv Dockerfile-aidreaming $repos_dir/Fooocus/
+mv requirements_aidreaming.txt $repos_dir/Fooocus/
 
 cd $repos_dir/Fooocus
 
@@ -52,21 +53,21 @@ if [ "$confirmation" = "yes" ]; then
     fi
 
     refiner_model='https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0_0.9vae.safetensors'
-    if [ -f ~/repos/Fooocus/models/checkpoints/sd_xl_base_1.0_0.9vae.safetensors ]; then 
-        echo "Base model already exists"; 
+    if [ -f ~/repos/Fooocus/models/checkpoints/sd_xl_refiner_1.0_0.9vae.safetensors ]; then 
+        echo "refiner model already exists"; 
     else 
-        echo "Base model does not exist."
+        echo "refiner model does not exist."
         echo "Downloading to ~/repos/Foocus/models/checkpoints"
         wget -P ~/repos/Fooocus/models/checkpoints $refiner_model
     fi
 
 
     lora_model='https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors'
-    if [ -f ~/repos/Fooocus/models/checkpoints/sd_xl_base_1.0_0.9vae.safetensors ]; then 
-        echo "Base model already exists"; 
+    if [ -f ~/repos/Fooocus/models/loras/sd_xl_offset_example-lora_1.0.safetensors ]; then 
+        echo "Lora model already exists"; 
     else 
-        echo "Base model does not exist."
-        echo "Downloading to ~/repos/Foocus/models/checkpoints"
+        echo "Lora model does not exist."
+        echo "Downloading to ~/repos/Foocus/models/loras"
         wget -P ~/repos/Fooocus/models/loras $lora_model
     fi
 elif [ "$confirmation" = "exit" ]; then
@@ -81,9 +82,21 @@ fi
 # Change the default number of images to generate per prompt to 1
 sed -i "s/image_number = gr.Slider(label='Image Number', minimum=1, maximum=32, step=1, value=2)/image_number = gr.Slider(label='Image Number', minimum=1, maximum=32, step=1, value=1)/" ~/repos/Fooocus/webui.py
 
+# Stop the launch script trying to download models
+sed -i "s/download_models()/#download_models()/" launch.py
+
+# enable cuda_malloc() function
+sed -i "s/# cuda_malloc()/cuda_malloc()/" launch.py
+
+# build container
+echo "Building the docker image"
+sleep 1
 sudo docker build -f $repos_dir/Fooocus//Dockerfile-aidreaming -t aidreaming:0.0.1 .
 
+echo ""
 echo "You can now run the container with"
 echo "docker run --gpus all -p 7860:7860 aidreaming:0.0.1"
+echo ""
 echo "This does not work in --detached mode."
+echo "if you want to volume mount the models into  container use -v $(pwd)/localmodel:/app/models/checkpoints"
 
